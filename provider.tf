4bf3f3ef338e6c576486461359c131d8f70227f3
jinnliong <chin.jinnliong@gmail.com>
2024-02-23 21:01:59 +0800

    change region variable in provider.tf

--- provider.tf
+++ provider.tf
@@ -10,5 +10,5 @@ terraform {
 
 provider "aws" {
   # The region is automatically determined from the AWS_DEFAULT_REGION environment variable
-  region = "${var.aws_region}"
+  region = var.aws_region
 }
