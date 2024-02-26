1d707a8b2004eee5702c478bfca54aa2104013ed
jinnliong <chin.jinnliong@gmail.com>
2024-02-26 17:40:43 +0800

    create new branch for terraform cloud

--- envs/dev/ec2/provider.tf
+++ envs/dev/ec2/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+}
+
 provider "aws" {
   # The region is automatically determined from the AWS_DEFAULT_REGION environment variable
   region = var.aws_region
